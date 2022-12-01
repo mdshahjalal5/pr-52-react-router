b6e4227c4d7e23a38f045a30331e3c768f5f166f
mdshahjalal5 <muhommodshahjalal@gmail.com>
2022-12-01 13:57:11 +0600

    freinddetails done

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import { Link, useLoaderData, useNavigate } from 'react-router-dom';
+import './PostDetails.css';
 const PostDetails = () => {
+    const details = useLoaderData();
+    const navigate = useNavigate();
+    const handleNavigate =() =>{
+        navigate(`/freind/${id}`)
+    }
+    const {id, title, body} = details;
     return (
-        <div>
-            post details
+        <div className='post-details'>
+            <h2>{title} </h2>
+            <p>{body} </p>
+            <button onClick={handleNavigate}>See more</button>
         </div>
     );
 };
